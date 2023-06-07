10f2c83cd9bf75909d43b549301289ca6174a761
Paichien <b8350580@gmail.com>
2023-06-08 01:29:38 +0800

    delete request revised

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -84,11 +84,12 @@ const handleNoteDelete = (e) => {
 
   const note = e.target;
   const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
-
   if (activeNote.id === noteId) {
     activeNote = {};
-    e.target.parentElement.remove();
   }
+
+  note.parentElement.remove();
+
 
   deleteNote(noteId).then(() => {
     getAndRenderNotes();
